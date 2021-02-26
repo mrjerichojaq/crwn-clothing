24757497bf597002961a36bfeb0117394a1efc0b
Jericho <jjaquebiz@gmail.com>
2021-02-25 20:48:06 -0700

    added remove for git

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -6,6 +6,7 @@ import "./homepage.styles.scss"
 const HomePage = () => {
     return (
         <div className="homepage">
+        <h1>Remove for git</h1>
             <div className="directory-menu">
                 <div className="menu-item">
                     <div className="content">
